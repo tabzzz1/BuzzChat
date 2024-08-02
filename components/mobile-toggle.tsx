712cc0a96e7b9406131d65d82d8e8d239337db4d
tabzzz <xpovo77@gmail.com>
2024-08-03 00:42:40 +0800

    update(Prisma): 新增建立对话所需的Model

--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { NavigationSidebar } from "@/components/navigation/navigation-sidebar";
 import { ServerSidebar } from "@/components/server/server-sidebar";
 
+// 聊天页面头部的菜单按钮，解决移动端或屏幕过小时左侧菜单的问题
 export const MobileToggle = ({
   serverId
 }: {
@@ -16,11 +17,13 @@ export const MobileToggle = ({
 }) => {
   return (
     <Sheet>
+      {/* 点击按钮触发 */}
       <SheetTrigger asChild>
         <Button variant="ghost" size="icon" className="md:hidden">
           <Menu />
         </Button>
       </SheetTrigger>
+      {/* 触发具体内容 */}
       <SheetContent side="left" className="p-0 flex gap-0">
         <div className="w-[72px]">
           <NavigationSidebar />
